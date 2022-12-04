f4193970713c2c1ac1f4a77961750c377e1d368d
jessica tanmateos <jessica_tanmateps@hotmail.com>
2022-12-05 00:15:08 +0100

    added more test

--- src/ts/__tests__/movieApp.test.ts
+++ src/ts/__tests__/movieApp.test.ts
@@ -33,29 +33,28 @@ test("should be able to call fn handleSubmit", () => {
 });
 //test2
 
-// describe("handleSubmit", () => {
-//   test("Should handle given data", async () => {
-//     //Arrange
-//     document.body.innerHTML = `<form id="searchForm">
-//     <input type="text" id="searchText" placeholder="Skriv titel här" />
-//     </form> <div id="movie-container"></div>`;
-//     // let searchText: string = (
-//     //   document.getElementById("searchText") as HTMLInputElement
-//     // ).value;
-//     // let container = document.getElementById(
-//     //   "movie-container"
-//     // ) as HTMLDivElement;
+describe("handleSubmit", () => {
+  beforeEach(() => {
+    jest.resetModules(); // clears cache for needed modules
+    jest.restoreAllMocks(); // restores the resolve-promise
+  });
+  test("Should handle given data", async () => {
+    //Arrange
+    document.body.innerHTML = `<form id="searchForm">
+    <input type="text" id="searchText" placeholder="Skriv titel här" />
+    </form> <div id="movie-container"></div>`;
 
-//     let spy = jest.spyOn(fn, "createHtml").mockReturnValue();
+    let spy = jest.spyOn(fn, "createHtml").mockReturnValue();
 
-//     //Act
-//     await fn.handleSubmit();
+    //Act
+    await fn.handleSubmit();
 
-//     // let movies = await getData(searchText);
-//     //Assert
-//     expect(spy).toHaveBeenCalled();
-//   });
-// });
+    // let movies = await getData(searchText);
+    //Assert
+    expect(spy).toHaveBeenCalled();
+    document.body.innerHTML = "";
+  });
+});
 
 // test 3
 test("should display message", async () => {
@@ -63,11 +62,6 @@ test("should display message", async () => {
   let container: HTMLDivElement = document.createElement(
     "div"
   ) as HTMLDivElement;
-  //   let noMessage: HTMLParagraphElement = document.createElement(
-  //     "p"
-  //   ) as HTMLParagraphElement;
-  //   noMessage.innerHTML = "Inga sökresultat att visa";
-  //   expect.assertions(1);
 
   //act
   fn.displayNoResult(container);
@@ -79,25 +73,31 @@ test("should display message", async () => {
 });
 
 //test 4
+describe("createHtml", () => {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.restoreAllMocks();
+  });
+  test("Should create HTML ", async () => {
+    //Arrange
 
-test("Should create HTML ", async () => {
-  //Arrange
+    document.body.innerHTML = `<div id="movie-container"></div>`;
+    let searchText: string = "hej";
 
-  document.body.innerHTML = `<div id="movie-container"></div>`;
-  let searchText: string = "hej";
+    let container: HTMLDivElement = document.getElementById(
+      "movie-container"
+    ) as HTMLDivElement;
+    let movies: IMovie[] = await getData(searchText);
 
-  let container: HTMLDivElement = document.getElementById(
-    "movie-container"
-  ) as HTMLDivElement;
-  let movies: IMovie[] = await getData(searchText);
+    //Act
 
-  //Act
+    fn.createHtml(movies, container);
 
-  fn.createHtml(movies, container);
+    //Asssert
 
-  //Asssert
-
-  expect(document.querySelectorAll("div.movie")?.length).toBe(3);
-  expect(document.querySelectorAll("h3")?.length).toBe(3);
-  expect(document.querySelectorAll("img")?.length).toBe(3);
+    expect(document.querySelectorAll("div.movie")?.length).toBe(3);
+    expect(document.querySelectorAll("h3")?.length).toBe(3);
+    expect(document.querySelectorAll("img")?.length).toBe(3);
+    document.body.innerHTML = "";
+  });
 });
